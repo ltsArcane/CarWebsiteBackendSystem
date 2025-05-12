1444aa0907c72b74f1e45b17a3784a9422494fb3
Aidan van Tonder <aidanvantonder@gmail.com>
2025-05-13 01:55:17 +0200

    Slight spacing change, please.

--- vehicles-api/src/main/java/com/udacity/vehicles_api/VehiclesApiApplication.java
+++ vehicles-api/src/main/java/com/udacity/vehicles_api/VehiclesApiApplication.java
@@ -5,9 +5,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VehiclesApiApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(VehiclesApiApplication.class, args);
-	}
-
+	public static void main(String[] args) { SpringApplication.run(VehiclesApiApplication.class, args); }
 }
