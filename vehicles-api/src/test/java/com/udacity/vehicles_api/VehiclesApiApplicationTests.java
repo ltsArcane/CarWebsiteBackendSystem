0dd1427e63717c2fdc2b2232d98f861ca32e28a6
Aidan van Tonder <aidanvantonder@gmail.com>
2025-05-13 01:31:44 +0200

    first commit

---
+++ vehicles-api/src/test/java/com/udacity/vehicles_api/VehiclesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udacity.vehicles_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehiclesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
