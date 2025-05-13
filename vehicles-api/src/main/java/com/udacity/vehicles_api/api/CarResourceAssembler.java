6dbead1ee84e92a274201f3f0608dc0e1f5444f8
Aidan van Tonder <aidanvantonder@gmail.com>
2025-05-13 02:38:28 +0200

    Import all of the base project files (and changes done to said files) from the last attempt.

---
+++ vehicles-api/src/main/java/com/udacity/vehicles_api/api/CarResourceAssembler.java
@@ -0,0 +1,23 @@
+package com.udacity.vehicles_api.api;
+
+import com.udacity.vehicles_api.domain.car.Car;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.stereotype.Component;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+
+/**
+ * Maps the CarController to the Car class using HATEOAS
+ */
+@Component
+public class CarResourceAssembler implements RepresentationModelAssembler<Car, EntityModel<Car>> {
+
+    @Override
+    public EntityModel<Car> toModel(Car car) {
+        return EntityModel.of(car,
+                linkTo(methodOn(CarController.class).get(car.getId())).withSelfRel(),
+                linkTo(methodOn(CarController.class).list()).withRel("cars"));
+
+    }
+}
---
+++ vehicles-api/src/main/java/com/udacity/vehicles_api/domain/Condition.java
@@ -0,0 +1,10 @@
+package com.udacity.vehicles_api.domain;
+
+/**
+ * Available values for condition of a given car.
+ */
+public enum Condition {
+
+    USED,
+    NEW;
+}
