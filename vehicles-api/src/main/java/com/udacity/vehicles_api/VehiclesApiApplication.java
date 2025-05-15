6eb82b6a7f441fbc1a04e52047272afc5d4fd5d6
Aidan van Tonder <aidanvantonder@gmail.com>
2025-05-15 19:03:21 +0200

    Move the CommandLineRunner "loadTestCars" across from main application to ApplicationTests.java - Adjust code and structure accordingly.

--- vehicles-api/src/main/java/com/udacity/vehicles_api/VehiclesApiApplication.java
+++ vehicles-api/src/main/java/com/udacity/vehicles_api/VehiclesApiApplication.java
@@ -1,7 +1,6 @@
 package com.udacity.vehicles_api;
 
 import org.modelmapper.ModelMapper;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
@@ -9,74 +8,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import com.udacity.vehicles_api.domain.car.Car;
-import com.udacity.vehicles_api.domain.car.Details;
-import com.udacity.vehicles_api.domain.manufacturer.Manufacturer;
-import com.udacity.vehicles_api.domain.manufacturer.ManufacturerRepository;
-import com.udacity.vehicles_api.client.maps.MapsClient;
-import com.udacity.vehicles_api.client.prices.PriceClient;
-import com.udacity.vehicles_api.domain.Condition;
-import com.udacity.vehicles_api.domain.Location;
-import com.udacity.vehicles_api.service.CarService;
 
 @SpringBootApplication
 @EnableJpaAuditing
 public class VehiclesApiApplication {
 	public static void main(String[] args) { SpringApplication.run(VehiclesApiApplication.class, args); }
-
-	/**
-     * Initializes the car manufacturers available to the Vehicle API.
-     * @param repository where the manufacturer information persists.
-     * @return the car manufacturers to add to the related repository
-     */
-
-    @Bean CommandLineRunner loadTestCars(ManufacturerRepository repository, CarService carService, PriceClient priceClient, MapsClient mapsClient) {
-        return args -> {
-            repository.save(new Manufacturer(100, "Audi"));
-            repository.save(new Manufacturer(101, "Ford"));
-            repository.save(new Manufacturer(102, "BMW"));
-
-            Manufacturer audi = repository.findById(100).orElseThrow();
-            Manufacturer ford = repository.findById(101).orElseThrow();
-            Manufacturer bmw = repository.findById(102).orElseThrow();
-
-            Car beforeSaveCar1 = new Car();
-            Car beforeSaveCar2 = new Car();
-            Car beforeSaveCar3 = new Car();
-
-            beforeSaveCar1.setCondition(Condition.NEW);
-            beforeSaveCar2.setCondition(Condition.NEW);
-            beforeSaveCar3.setCondition(Condition.NEW);
-
-            beforeSaveCar1.setDetails(new Details("SUV", "X5", bmw, 4, "Diesel", "3.0L V6", 20000, 2021, 2021, "White"));
-            beforeSaveCar2.setDetails(new Details("SUV", "X5", audi, 4, "Diesel", "3.0L V6", 20000, 2021, 2021, "White"));
-            beforeSaveCar3.setDetails(new Details("SUV", "X5", ford, 4, "Diesel", "3.0L V6", 20000, 2021, 2021, "White"));
-
-            Location location1 = new Location(12.3456, -65.4321);
-            Location location2 = new Location(-78.9012, 21.0987);
-            Location location3 = new Location(34.5678, -87.6543);
-
-            location1 = mapsClient.getAddress(location1);
-            location2 = mapsClient.getAddress(location2);
-            location3 = mapsClient.getAddress(location3);
-
-            beforeSaveCar1.setLocation(location1);
-            beforeSaveCar2.setLocation(location2);
-            beforeSaveCar3.setLocation(location3);
-
-            Car afterSaveCar1 = carService.save(beforeSaveCar1);
-            Car afterSaveCar2 = carService.save(beforeSaveCar2);
-            Car afterSaveCar3 = carService.save(beforeSaveCar3);
-
-            Long id1 = afterSaveCar1.getId();
-            Long id2 = afterSaveCar2.getId();
-            Long id3 = afterSaveCar3.getId();
-
-            afterSaveCar1.setPrice(priceClient.getPrice(id1));
-            afterSaveCar2.setPrice(priceClient.getPrice(id2));
-            afterSaveCar3.setPrice(priceClient.getPrice(id3));
-        };
-    }
 
     @Bean ModelMapper modelMapper() { return new ModelMapper(); }
 
--- vehicles-api/src/test/java/com/udacity/vehicles_api/VehiclesApiApplicationTests.java
+++ vehicles-api/src/test/java/com/udacity/vehicles_api/VehiclesApiApplicationTests.java
@@ -3,11 +3,67 @@ package com.udacity.vehicles_api;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.udacity.vehicles_api.client.maps.MapsClient;
+import com.udacity.vehicles_api.client.prices.PriceClient;
+import com.udacity.vehicles_api.domain.Condition;
+import com.udacity.vehicles_api.domain.Location;
+import com.udacity.vehicles_api.domain.car.Car;
+import com.udacity.vehicles_api.domain.car.Details;
+import com.udacity.vehicles_api.domain.manufacturer.Manufacturer;
+import com.udacity.vehicles_api.domain.manufacturer.ManufacturerRepository;
+import com.udacity.vehicles_api.service.CarService;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @SpringBootTest
 class VehiclesApiApplicationTests {
+
+	private final ManufacturerRepository manufacturerRepository;
+    private final CarService carService;
+    private final PriceClient priceClient;
+    private final MapsClient mapsClient;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test //* Does not use any proper mockito checks, but does test the functionality of the Vehicles API. Check for error messages. */
+    void testCarCreationAndEnrichment() {
+        manufacturerRepository.save(new Manufacturer(100, "Audi"));
+        manufacturerRepository.save(new Manufacturer(101, "Ford"));
+        manufacturerRepository.save(new Manufacturer(102, "BMW"));
+
+        Manufacturer audi = manufacturerRepository.findById(100).orElseThrow();
+        Manufacturer ford = manufacturerRepository.findById(101).orElseThrow();
+        Manufacturer bmw  = manufacturerRepository.findById(102).orElseThrow();
+
+        Car car1 = new Car();
+        Car car2 = new Car();
+        Car car3 = new Car();
+
+        car1.setCondition(Condition.NEW);
+        car2.setCondition(Condition.NEW);
+        car3.setCondition(Condition.NEW);
+
+        car1.setDetails(new Details("SUV", "X5", bmw, 4, "Diesel", "3.0L V6", 20000, 2021, 2021, "White"));
+        car2.setDetails(new Details("SUV", "X5", audi, 4, "Diesel", "3.0L V6", 20000, 2021, 2021, "White"));
+        car3.setDetails(new Details("SUV", "X5", ford, 4, "Diesel", "3.0L V6", 20000, 2021, 2021, "White"));
+
+        Location loc1 = mapsClient.getAddress(new Location(12.3456, -65.4321));
+        Location loc2 = mapsClient.getAddress(new Location(-78.9012, 21.0987));
+        Location loc3 = mapsClient.getAddress(new Location(34.5678, -87.6543));
+
+        car1.setLocation(loc1);
+        car2.setLocation(loc2);
+        car3.setLocation(loc3);
+
+        Car saved1 = carService.save(car1);
+        Car saved2 = carService.save(car2);
+        Car saved3 = carService.save(car3);
+
+        saved1.setPrice(priceClient.getPrice(saved1.getId()));
+        saved2.setPrice(priceClient.getPrice(saved2.getId()));
+        saved3.setPrice(priceClient.getPrice(saved3.getId()));
+    }
 }
