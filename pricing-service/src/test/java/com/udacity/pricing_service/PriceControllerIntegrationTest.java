090a9f694866d4c891c395722c2dd4e6a894561f
Aidan van Tonder <aidanvantonder@gmail.com>
2025-05-15 23:02:10 +0200

    Improve the Pricing Service's PriceControllerIntegrationTest. - Use JacksonTester instead of ObjectMapper for PriceControllerIntegrationTests. - Ensure that we use object getters for comparing jsonpaths to values instead of hard-coding in the values within the comparisons.

--- pricing-service/src/test/java/com/udacity/pricing_service/PriceControllerIntegrationTest.java
+++ pricing-service/src/test/java/com/udacity/pricing_service/PriceControllerIntegrationTest.java
@@ -1,41 +1,41 @@
 package com.udacity.pricing_service;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import static org.hamcrest.Matchers.containsString;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.udacity.pricing_service.entities.Price;
-import lombok.RequiredArgsConstructor;
 import java.math.BigDecimal;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@RequiredArgsConstructor
+@AutoConfigureJsonTesters
 public class PriceControllerIntegrationTest {
 
-    private MockMvc mockMvc;
-    private ObjectMapper objectMapper;
+    @Autowired private MockMvc mockMvc;
+    @Autowired private JacksonTester<Price> json;
 
     @Test
     void testGetPriceEndpoint() throws Exception {
-        Long vehicleId = 1L;
-        Price price = new Price(vehicleId, "USD", new BigDecimal("10000.00"));
+        Price price = new Price(1L, "USD", new BigDecimal("10000.00"));
 
-        mockMvc.perform(post("/prices").contentType("application/json").content(objectMapper.writeValueAsString(price)))
+        mockMvc.perform(post("/prices").contentType("application/json").content(json.write(price).getJson()))
                 .andExpect(status().isCreated())
-                .andExpect(header().string("Location", containsString("/prices/" + vehicleId)));
+                .andExpect(header().string("Location", containsString("/prices/" + price.getVehicleId())));
     
-        mockMvc.perform(get("/prices/{id}", vehicleId))
+        mockMvc.perform(get("/prices/{id}", price.getVehicleId()))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType("application/hal+json")) // HATEOAS
-                .andExpect(jsonPath("$.currency").value("USD"))
-                .andExpect(jsonPath("$.price").value(10000.00))
-                .andExpect(jsonPath("$._links.self.href").value(containsString("/prices/" + vehicleId)));
+                .andExpect(jsonPath("$.currency").value(price.getCurrency()))
+                .andExpect(jsonPath("$.price").value(price.getPrice()))
+                .andExpect(jsonPath("$._links.self.href").value(containsString("/prices/" + price.getVehicleId())));
     }
 }
