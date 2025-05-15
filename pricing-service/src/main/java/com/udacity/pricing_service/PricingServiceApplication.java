6f69d15a07fbf4e319f7bdba90993fea1bb11a80
Aidan van Tonder <aidanvantonder@gmail.com>
2025-05-15 04:05:54 +0200

    Finish converting pricing-service into a microservice. - Remove the controllers and services. - Move the test price data creation to a CommandLineRunner (using h2, so will be wiped each time) - Depend on Spring Boot Data REST. - Re-adjust Price such that it uses vehicleId as it's @Id instead of generating an Id on save (Only realised now that this was the intended design).
    Note: Will have to adjust Vehicle API's client to use new endpoints and corrected parameters.

--- pricing-service/src/main/java/com/udacity/pricing_service/PricingServiceApplication.java
+++ pricing-service/src/main/java/com/udacity/pricing_service/PricingServiceApplication.java
@@ -1,11 +1,29 @@
 package com.udacity.pricing_service;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.LongStream;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+import com.udacity.pricing_service.entities.Price;
+import com.udacity.pricing_service.repositories.PriceRepository;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class PricingServiceApplication {
 	public static void main(String[] args) { SpringApplication.run(PricingServiceApplication.class, args); }
+
+	@Bean CommandLineRunner preloadPrices(PriceRepository repository) {
+		return args -> { LongStream.range(1, 20).forEach(i -> { Price price = new Price(i, "USD", randomPrice()); repository.save(price); }); };
+	}
+
+	private BigDecimal randomPrice() {
+		return new BigDecimal(ThreadLocalRandom.current().nextDouble(1, 5)).multiply(BigDecimal.valueOf(5000)).setScale(2, RoundingMode.HALF_UP);
+	}
 }
--- pricing-service/src/main/java/com/udacity/pricing_service/entities/Price.java
+++ pricing-service/src/main/java/com/udacity/pricing_service/entities/Price.java
@@ -2,25 +2,16 @@ package com.udacity.pricing_service.entities;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
-@Data @AllArgsConstructor @Entity
+@Data @NoArgsConstructor @AllArgsConstructor @Entity
 public class Price {
-
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) private Long id;
+    @Id private Long vehicleId;
     private String currency;
     private BigDecimal price;
-    private Long vehicleId;
-
-    public Price(String currency, BigDecimal price, Long vehicleId) { // Custom constructor.
-        this.currency = currency;
-        this.price = price;
-        this.vehicleId = vehicleId;
-    }
 }
