ab30395c2b9cd8ed91b6e27bcde5d3c61a86a52d
Aidan van Tonder <aidanvantonder@gmail.com>
2025-05-15 04:12:23 +0200

    Change coding style such that everything is more flat / one-lined.

--- vehicles-api/src/main/java/com/udacity/vehicles_api/client/maps/MapsClient.java
+++ vehicles-api/src/main/java/com/udacity/vehicles_api/client/maps/MapsClient.java
@@ -13,17 +13,12 @@ import org.springframework.web.reactive.function.client.WebClient;
  */
 @Component
 public class MapsClient {
-
     private static final Logger log = LoggerFactory.getLogger(MapsClient.class);
-
     private final WebClient client;
     private final ModelMapper mapper;
 
-    public MapsClient(WebClient maps,
-            ModelMapper mapper) {
-        this.client = maps;
-        this.mapper = mapper;
-    }
+    // Cannot use required args constructor due to Spring Boot requesting that the WebClient be named maps.
+    public MapsClient(WebClient maps, ModelMapper mapper) { this.client = maps; this.mapper = mapper; }
 
     /**
      * Gets an address from the Maps client, given latitude and longitude.
@@ -33,18 +28,8 @@ public class MapsClient {
      */
     public Location getAddress(Location location) {
         try {
-            Address address = client
-                    .get()
-                    .uri(uriBuilder -> uriBuilder
-                            .path("/maps/")
-                            .queryParam("lat", location.getLat())
-                            .queryParam("lon", location.getLon())
-                            .build()
-                    )
-                    .retrieve().bodyToMono(Address.class).block();
-
+            Address address = client.get().uri(uriBuilder -> uriBuilder.path("/maps/").queryParam("lat", location.getLat()).queryParam("lon", location.getLon()).build()).retrieve().bodyToMono(Address.class).block();
             mapper.map(Objects.requireNonNull(address), location);
-
             return location;
         } catch (Exception e) {
             log.warn("Map service is down");
