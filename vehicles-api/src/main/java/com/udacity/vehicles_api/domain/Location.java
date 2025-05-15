0a7cd0891d1a049bb54f1e4186f68022ec125cd2
Aidan van Tonder <aidanvantonder@gmail.com>
2025-05-15 08:15:03 +0200

    Ensure Location has a no-arg constructor so Jackson can do it's thing when it comes to location-service (boogle maps)

--- vehicles-api/src/main/java/com/udacity/vehicles_api/domain/Location.java
+++ vehicles-api/src/main/java/com/udacity/vehicles_api/domain/Location.java
@@ -3,21 +3,22 @@ package com.udacity.vehicles_api.domain;
 import jakarta.persistence.Embeddable;
 import jakarta.persistence.Transient;
 import jakarta.validation.constraints.NotNull;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * Stores information about a given location.
  * Latitude and longitude must be provided, while other location information must be gathered each time from the maps API.
  * Since Latitude and Longitude are required, this class only has a required args constructor.
  */
-@Embeddable @RequiredArgsConstructor
+@Embeddable @Data @NoArgsConstructor
 public class Location {
-    @Getter @NotNull private final Double lat;
-    @Getter @NotNull private final Double lon;
-    @Getter @Setter @Transient private String address;
-    @Getter @Setter @Transient private String city;
-    @Getter @Setter @Transient private String state;
-    @Getter @Setter @Transient private String zip;
+    @NotNull private Double lat;
+    @NotNull private Double lon;
+    @Transient private String address;
+    @Transient private String city;
+    @Transient private String state;
+    @Transient private String zip;
+
+    public Location(Double lat, Double lon) { this.lat = lat; this.lon = lon; }
 }
